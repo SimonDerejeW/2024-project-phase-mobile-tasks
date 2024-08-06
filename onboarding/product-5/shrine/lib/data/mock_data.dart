48957bc33c2727d88cce9330e9d418c074076d19
Simon <simondereje1@gmail.com>
2024-08-06 15:48:19 +0300

    shrine app

---
+++ onboarding/product-5/shrine/lib/data/mock_data.dart
@@ -0,0 +1,23 @@
+final List<Map> mockData = [
+    {"image": "assets/blue_dunk.png", "title": "Blue Dunk", "price": 50.99},
+    {"image": "assets/dunk.png", "title": "Off-White Dunks", "price": 75.99},
+    {"image": "assets/kd.png", "title": "KD 16's", "price": 42.00},
+    {"image": "assets/sb-dunk.png", "title": "SB Dunk", "price": 30.99},
+    {"image": "assets/blue_dunk.png", "title": "Blue Dunk", "price": 50.99},
+    {"image": "assets/dunk.png", "title": "Off-White Dunks", "price": 75.99},
+    {"image": "assets/kd.png", "title": "KD 16's", "price": 42.00},
+    {"image": "assets/sb-dunk.png", "title": "SB Dunk", "price": 30.99},
+    {"image": "assets/blue_dunk.png", "title": "Blue Dunk", "price": 50.99},
+    {"image": "assets/dunk.png", "title": "Off-White Dunks", "price": 75.99},
+    {"image": "assets/kd.png", "title": "KD 16's", "price": 42.00},
+    {"image": "assets/sb-dunk.png", "title": "SB Dunk", "price": 30.99},
+    {"image": "assets/blue_dunk.png", "title": "Blue Dunk", "price": 50.99},
+    {"image": "assets/dunk.png", "title": "Off-White Dunks", "price": 75.99},
+    {"image": "assets/kd.png", "title": "KD 16's", "price": 42.00},
+    {"image": "assets/sb-dunk.png", "title": "SB Dunk", "price": 30.99},
+    {"image": "assets/blue_dunk.png", "title": "Blue Dunk", "price": 50.99},
+    {"image": "assets/dunk.png", "title": "Off-White Dunks", "price": 75.99},
+    {"image": "assets/kd.png", "title": "KD 16's", "price": 42.00},
+    {"image": "assets/sb-dunk.png", "title": "SB Dunk", "price": 30.99},
+    
+  ];
---
+++ onboarding/product-5/shrine/lib/widgets/product_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final double price;
+  const ProductCard(
+      {super.key,
+      required this.image,
+      required this.price,
+      required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+          onTap: () {
+            debugPrint("Card Tapped");
+          },
+          splashColor: Colors.purple[100],
+          child: SizedBox(
+            width: 200,
+            height: 200,
+            child: Column(
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: AspectRatio(aspectRatio: 18.0/11.0, child: Image.asset(image)),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        title,
+                        style:const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      const Spacer(),
+                      Text('\$$price')
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
